5541280e6cdfb5b481bd511f77d0dbc8bf57b7b3
Romain Guillaumé <romaing.bis@gmail.com>
2022-06-20 21:08:37 +0200

    ...

--- DoG_interactive_parameters.py
+++ DoG_interactive_parameters.py
@@ -11,9 +11,9 @@ from matplotlib.widgets import Slider
 
 
 # Difference of Gaussians : 
-# - fait la différence de deux flous gaussiens (flou gaussien = convolution de l'image avec une gaussienne en 2D)
-# - applique un flou gaussien sur le résultat (la différence)
-# - fait un masque pour prendre que les valeurs assez hautes
+# - faire la différence de deux flous gaussiens (flou gaussien = convolution de l'image avec une gaussienne en 2D)
+# - appliquer un flou gaussien sur le résultat (la différence)
+# - faire un masque pour prendre que les valeurs assez hautes (assez proches du max de l'image)
 
 
 # L'idée de la différence de gaussienne est d'obtenir la convolution de l'image par une
@@ -22,7 +22,7 @@ from matplotlib.widgets import Slider
 # spatiales. Les fréquences spatiales caractérisent la vitesse de variation des pixels sur une image.
 # En choisissant le bon passe-bande, on peut garder les motifs qui correspondent au petites
 # poussières et impuretés sur notre image, et supprimer le reste. On simplifie ainsi notre
-# image tout en gardant assez pour pouvoir déterminer la translation.
+# image tout en gardant assez d'information pour pouvoir déterminer la translation.
 
 
 
@@ -49,7 +49,8 @@ def DoG(im,
 
 
 
-
+# Tout ce qui est dessous concerne la mise en place de 
+# la fenetre interactive (pas interessant à lire)
 if __name__ == "__main__":
 
     im = [cv.imread(f"SE{i}.tif", cv.IMREAD_UNCHANGED) for i in range(1,6)]
@@ -68,53 +69,59 @@ if __name__ == "__main__":
     mask1 = DoG(im[0], kernel_shape, sigma_inf, sigma_sup, (0,0), last_blur_sigma, tolerance_threshold)
     mask2 = DoG(im[1], kernel_shape, sigma_inf, sigma_sup, (0,0), last_blur_sigma, tolerance_threshold)
 
-    fig, ax = plt.subplots(1, 2, figsize=(10, 8))
+    fig, ax = plt.subplots(1, 2, figsize=(11, 6))
     fig.suptitle("Les paramètres initiaux nous semblent pas mal")
     fig.tight_layout()
-    plot_im1 = ax[0].imshow(mask1) ; ax[0].set_title("DoG sur image 1")
+    plot_im1 = ax[0].imshow(mask1)
+    contours1, hierarchy1 = cv.findContours(mask1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    ax[0].set_title(f"DoG sur image 1 : {len(contours1)} composantes connexes")
+
     plot_im2 = ax[1].imshow(mask2) ; ax[1].set_title("DoG sur image 2")
+    contours2, hierarchy2 = cv.findContours(mask2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    ax[1].set_title(f"DoG sur image 2 : {len(contours2)} composantes connexes")
+
     plt.subplots_adjust(bottom=0.25)
 
-    ax_last = plt.axes([0.1, 0.05, 0.25, 0.03])
+    ax_last = plt.axes([0.7, 0.05, 0.25, 0.03])
     last_blur_slider = Slider(
         ax=ax_last,
-        label='last blur',
+        label='écart-type du flou gaussien appliqué à la fin',
         valmin=1,
         valmax=30,
         valinit=last_blur_sigma,
     )
 
-    ax_sigma_inf = plt.axes([0.1, 0.11, 0.25, 0.03])
+    ax_sigma_inf = plt.axes([0.15, 0.11, 0.2, 0.03])
     sigma_inf_slider = Slider(
         ax=ax_sigma_inf,
-        label='sigma inf',
+        label="sigma_inf",
         valmin=1,
         valmax=100,
         valinit=sigma_inf,
     )
 
-    ax_sigma_diff = plt.axes([0.1, 0.17, 0.25, 0.03])
+    ax_sigma_diff = plt.axes([0.15, 0.05, 0.2, 0.03])
     sigma_diff_slider = Slider(
         ax=ax_sigma_diff,
-        label='diff sigma',
+        label='sigma_sup - sigma_inf',
         valmin=0.01,
         valmax=15,
         valinit=sigma_diff,
     )
 
-    ax_tol = plt.axes([0.6, 0.05, 0.25, 0.03])
+    ax_tol = plt.axes([0.7, 0.17, 0.25, 0.03])
     threshold_slider = Slider(
         ax=ax_tol,
-        label='threshold',
+        label='seuil de tolérance du masque (voir le code de la fonction DoG)',
         valmin=10,
         valmax=40000,
         valinit=tolerance_threshold,
     )
 
-    ax_ksize = plt.axes([0.6, 0.11, 0.25, 0.03])
+    ax_ksize = plt.axes([0.7, 0.11, 0.25, 0.03])
     ksize_slider = Slider(
         ax=ax_ksize,
-        label='kernel size',
+        label='taille du kernel (le kernel est carré)',
         valmin=3,
         valmax=60,
         valinit=kernel_shape[0],
@@ -128,9 +135,9 @@ if __name__ == "__main__":
                 kernel_shape, sigma_inf_slider.val, sigma_inf_slider.val + sigma_diff_slider.val, 
                 (0,0), last_blur_slider.val, 
                 threshold_slider.val)    
-        contours1, hierarchy1 = cv.findContours(mask1, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+        contours1, hierarchy1 = cv.findContours(mask1, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
         
-        ax[0].set_title(len(contours1))
+        ax[0].set_title(f"DoG sur image 1 : {len(contours1)} composantes connexes")
         plot_im1.set_data(mask1)
         
         
@@ -138,9 +145,9 @@ if __name__ == "__main__":
                 kernel_shape, sigma_inf_slider.val, sigma_inf_slider.val + sigma_diff_slider.val, 
                 (0,0), last_blur_slider.val, 
                 threshold_slider.val)
-        contours2, hierarchy2 = cv.findContours(mask2, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+        contours2, hierarchy2 = cv.findContours(mask2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
         
-        ax[1].set_title(len(contours2))
+        ax[1].set_title(f"DoG sur image 2 : {len(contours2)} composantes connexes")
         plot_im2.set_data(mask2)
         
         fig.canvas.draw_idle()
